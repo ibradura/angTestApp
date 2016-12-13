5c52848bd76ee784303ed7d9932662d4b0997e7f
ibradura <ibrahimduran@hotmail.com.tr>
2016-12-13 15:32:30 +0200

    first

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Başlığımız buraya';
+  content = 'Bu bir content yazısıdır. Ve bu yazı dummy olarak basılmıştır. Bu bir content yazısıdır. Ve bu yazı dummy olarak basılmıştır. Bu bir content yazısıdır. Ve bu yazı dummy olarak basılmıştır. Bu bir content yazısıdır. Ve bu yazı dummy olarak basılmıştır. Bu bir content yazısıdır. Ve bu yazı dummy olarak basılmıştır. Bu bir content yazısıdır. Ve bu yazı dummy olarak basılmıştır. Bu bir content yazısıdır. Ve bu yazı dummy olarak basılmıştır.';
+}
